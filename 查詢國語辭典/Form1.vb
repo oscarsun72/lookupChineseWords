e4ca2670f3c312dc4074511a0502f53d92710a89
守真 孫 <oscarsun72@hotmail.com>
2021-04-17 22:13:01 +0800

    OK 新增參考資料 https://ithelp.ithome.com.tw/questions/10197561

--- 查詢國語辭典/Form1.vb
+++ 查詢國語辭典/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
     Dim wx As String,
-        defaultBrowserFullname As String =
+        browserApp As String =
         Replace(DefaultBrowser, " -- ""%1""", "").Trim()
     Function 查詢字串轉換_國語會碼(w As String) 'Big5碼
         Dim u8 As System.Text.Encoding = System.Text.Encoding.GetEncoding("big5") 'https://msdn.microsoft.com/zh-tw/library/system.text.encoding(v=vs.110).aspx
@@ -32,19 +32,19 @@
 
     Sub 查詢國語辭典()
         Const url = "http://dict.revised.moe.edu.tw/cbdic/search.htm"
-        Process.Start(defaultBrowserFullname, url)
+        Process.Start(browserApp, url)
 #Region "oldCode"
         'On Error GoTo Error_GetUserAddress
         '顯示隱藏漢語大詞典()
         'If Screen.ActiveControl.Sellength > 0 Then DoCmd.RunCommand(acCmdCopy)
         'Clipboard.SetText(x)
         'If Len(x) > 1 Then
-        '    Shell(Replace(DefaultBrowserFullname, """%1", "http://dict.revised.moe.edu.tw/cgi-bin/newDict/dict.sh?idx=dict.idx&cond=" & 查詢字串轉換_國語會碼(x) & "&pieceLen=50&fld=1&cat=&imgFont=1"))
+        '    Shell(Replace(browserApp, """%1", "http://dict.revised.moe.edu.tw/cgi-bin/newDict/dict.sh?idx=dict.idx&cond=" & 查詢字串轉換_國語會碼(x) & "&pieceLen=50&fld=1&cat=&imgFont=1"))
         'Else
-        '    Shell(Replace(DefaultBrowserFullname, """%1", "http://dict.revised.moe.edu.tw/cgi-bin/newDict/dict.sh?idx=dict.idx&cond=^" & 查詢字串轉換_國語會碼(x) & "$&pieceLen=50&fld=1&cat=&imgFont=1"))
+        '    Shell(Replace(browserApp, """%1", "http://dict.revised.moe.edu.tw/cgi-bin/newDict/dict.sh?idx=dict.idx&cond=^" & 查詢字串轉換_國語會碼(x) & "$&pieceLen=50&fld=1&cat=&imgFont=1"))
         'End If
 
-        'Shell(Replace(DefaultBrowserFullname, """%1", "http://dict.revised.moe.edu.tw/cbdic/search.htm"))
+        'Shell(Replace(browserApp, """%1", "http://dict.revised.moe.edu.tw/cbdic/search.htm"))
 
 
         'If GetUserAddress(x) = True Then
@@ -59,31 +59,31 @@
     Sub 查詢漢典()
         Dim url = "https://www.google.com.tw/search?q=site:http://www.zdic.net/+" & wx &
                 " " & "http://www.zdic.net/search/?q=" & wx
-        Process.Start(defaultBrowserFullname, url)
+        Process.Start(browserApp, url)
 #Region "舊碼"
-        'Shell(Replace(defaultBrowserFullname, """%1", "https://www.google.com.tw/search?q=site:http://www.zdic.net/+" & wx))
-        'Shell(Replace(defaultBrowserFullname, """%1", "http://www.zdic.net/search/?q=" & wx)) 'http://www.zdic.net/search/?q=%E8%AD%A6%E7%9B%AE&c=2
+        'Shell(Replace(browserApp, """%1", "https://www.google.com.tw/search?q=site:http://www.zdic.net/+" & wx))
+        'Shell(Replace(browserApp, """%1", "http://www.zdic.net/search/?q=" & wx)) 'http://www.zdic.net/search/?q=%E8%AD%A6%E7%9B%AE&c=2
 #End Region
     End Sub
     Sub 查詢國學大師汉语字典(x As String) 'http://www.guoxuedashi.net/zidian/93F5.html
         Dim url As String = "http://www.guoxuedashi.net/so.php?sokeytm=" & wx & "&ka=100&submit=" &
             " " & "http://tw.ichacha.net/zaoju/" & wx & ".html"
-        Process.Start(defaultBrowserFullname, url)
+        Process.Start(browserApp, url)
 #Region "舊碼"
         'Dim u8 As System.Text.Encoding = System.Text.Encoding.Unicode
         'Dim bytes As Byte() = u8.GetBytes(x)
         'Const HDurl As String = "http://www.guoxuedashi.net/so.php?sokeytm="
-        'Shell(Replace(defaultBrowserFullname, """%1", HDurl & wx & "&ka=100&submit="))
-        'Shell(Replace(defaultBrowserFullname, """%1", "http://tw.ichacha.net/zaoju/" & wx & ".html")) '查詢查查網造句 2016/10/11
+        'Shell(Replace(browserApp, """%1", HDurl & wx & "&ka=100&submit="))
+        'Shell(Replace(browserApp, """%1", "http://tw.ichacha.net/zaoju/" & wx & ".html")) '查詢查查網造句 2016/10/11
 #End Region
     End Sub
     Private Sub Form1_Load(sender As Object, e As System.EventArgs) Handles Me.Load
         Dim qx As String = Clipboard.GetText
         If qx = "" Then Exit Sub
         wx = 查詢字串轉換_網路碼(qx)
-        If Not defaultBrowserFullname.IndexOf("iexplore") Then
+        If Not browserApp.IndexOf("iexplore") Then
             Dim bChrom As New BrowserChrome
-            defaultBrowserFullname = bChrom.ChromeAppFileName
+            browserApp = bChrom.ChromeAppFileName
         End If
         If Len(qx) > 1 Then 查詢國學大師汉语字典(qx)
         查詢漢典()
